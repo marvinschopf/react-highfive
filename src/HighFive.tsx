bd5e010fb2478d50f89b4ae79c93a69080b19d18
Marvin Schopf <marvin@schopf.biz>
2021-02-04 22:38:00 +0100

    Prettify code

--- src/HighFive.tsx
+++ src/HighFive.tsx
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  *  react-highfive
  *  Copyright (C) 2021 Marvin Schopf
  *
@@ -14,7 +14,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- * 
+ *
  **/
 
 import React, { Component } from "react";
