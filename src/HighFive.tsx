eb6cda34c2fd87189dfd52397262b9f9de2f2c1d
Marvin Schopf <marvin@schopf.biz>
2021-02-05 00:13:59 +0100

    Fixed copyright tag in JSDoc

--- src/HighFive.tsx
+++ src/HighFive.tsx
@@ -15,7 +15,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  * 
- *  @copyright 2021 Marvin Schopf
+ *  @copyright Marvin Schopf 2021
  *  @license Apache-2.0
  *
  **/
